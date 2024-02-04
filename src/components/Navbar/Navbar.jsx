6a4d06a9898454e6844022247ec19d4007fc2bee
Karanjsk2k01 <karanvarun14501@gmail.com>
2024-02-04 13:20:05 +0530

    email verification

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,59 @@
 import React from 'react'
 import classes from './Navbar.module.css';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+  const history = useNavigate()
+
+  const verifyEmailHandler = async (e) => {
+
+    e.preventDefault();
+
+    let idToken = localStorage.getItem('token');
+
+    if (idToken === 'undefined' || idToken === null) {
+      alert('Please Login to continue!!');
+      history('/Auth')
+    }
+
+    try {
+      let url = 'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBxqP5SfeSM8fgzRarxiwEgZ4kJ5v7JZsA'
+
+      let res = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          requestType: "VERIFY_EMAIL",
+          idToken: idToken
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+
+      if (!res.ok) {
+        throw new Error('Something went wrong try after some time....')
+      }
+
+      let data = await res.json()
+
+      console.log(data)
+    }
+    catch (error) {
+      alert(error.message)
+    }
+  }
+
+
+
   return (
     <div className={classes.navbar}>
       <div className={classes.right}>
         <span>Expense Tracker</span>
       </div>
       <div className={classes.left}>
+        <button type='button' className={classes.button} onClick={verifyEmailHandler}>Verify Email</button>
         <span style={{ padding: '0 20px' }}>Your Profile is incomplete -</span>
         <Link to="/Profile" className={classes.link}>Please Complete your profile</Link>
         <Link to="/Auth" className={classes.auth}>Login</Link>
